bacb0f39894f20c5a1c47c82a7d905a68cd41d75
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2025-01-11 16:33:06 +0100

    Fix editing of other user's post

--- src/pages/single-post/connectors/post-connector.tsx
+++ src/pages/single-post/connectors/post-connector.tsx
@@ -1,5 +1,6 @@
 import { PostEditor } from '@/components/post-editor/post-editor';
 import { usePost } from '../../../api/posts/use-post';
+import { useUserContext } from '@/context/user-context';
 
 type Props = {
     id: string;
@@ -10,6 +11,15 @@ export const PostConnector = ({ id, isEditing }: Props) => {
     const { data } = usePost({
         id,
     });
+
+    const user = useUserContext();
+
+    if (user?.id !== data.data.author.id && isEditing) {
+        throw new Response('You do not have permission to view this page.', {
+            status: 403,
+            statusText: 'You do not have permission to view this page.',
+        });
+    }
 
     return <PostEditor post={data.data} disabled={!isEditing} />;
 };
