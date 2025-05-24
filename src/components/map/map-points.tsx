f174ec0824c2e8872d3b56303d37d54c3ffe8aaa
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2025-05-24 13:52:41 +0200

    sort translations

--- src/components/map/map-points.tsx
+++ src/components/map/map-points.tsx
@@ -6,6 +6,8 @@ import { MapPinIcon, Receipt } from 'lucide-react';
 import { formatPrice } from '../../lib/formatters';
 import { useTranslation } from 'react-i18next';
 import { ImageCarousel } from '../post-editor/components/image-carousel';
+import { Button } from '../ui/button';
+import { Link } from '@tanstack/react-router';
 
 interface Props {
     posts: Post[];
@@ -48,20 +50,36 @@ export const MapPoints = ({ posts }: Props) => {
                                 title={post.title}
                                 navCarousel={false}
                             />
-                            <h1 className="font-semibold line-clamp-2 leading-tight px-2 text-base mb-2">
+                            <h1 className="font-semibold line-clamp-2 leading-tight px-3 text-base mb-1">
                                 {post.title}
                             </h1>
-                            <div className="flex items-center px-2">
+                            <div className="flex items-center px-3 gap-2">
                                 <MapPinIcon className="size-4" />
-                                <div className="line-clamp-1 px-2 text-sm">
+                                <div className="line-clamp-1 text-sm">
                                     {post.address}
                                 </div>
                             </div>
-                            <div className="flex items-center px-2 mb-3">
+                            <div className="flex items-center px-3 mb-2 gap-2">
                                 <Receipt className="size-4" />
-                                <div className="line-clamp-1 px-2 text-sm">
+                                <div className="line-clamp-1 text-sm">
                                     {formatPrice(post.price, post.type, t)}
                                 </div>
+                            </div>
+                            <div className="flex w-full justify-end mb-3 px-3">
+                                <Button
+                                    id={`post-${post.id}-details`}
+                                    asChild
+                                    className="!text-primary-foreground font-medium"
+                                >
+                                    <Link
+                                        to="/posts/$postId"
+                                        params={{
+                                            postId: `${post.id}`,
+                                        }}
+                                    >
+                                        {t('View details')}
+                                    </Link>
+                                </Button>
                             </div>
                         </div>
                     </Popup>
