f6974508be1fe58f21d0dd036eadadf949ae0770
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2025-11-21 23:30:13 +0100

    another one

--- src/components/global-loader.tsx
+++ src/components/global-loader.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useTheme } from './theme-provider';
 
 export const GlobalLoader = () => {
     const [isDarkTheme, setIsDarkTheme] = useState(false);
-    const theme = useTheme();
 
     useEffect(() => {
         setIsDarkTheme(
             window.document.documentElement.classList.contains('dark'),
         );
-    }, [theme]);
+    }, []);
 
     return (
         <div className="flex items-center justify-center h-screen fixed top-0 left-0 w-full z-10">
