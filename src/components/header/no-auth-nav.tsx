3791e965b979dbf488c7c0959291123f5868b49e
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2026-01-24 19:56:29 +0100

    Fix mobile layout

--- src/components/header/no-auth-nav.tsx
+++ src/components/header/no-auth-nav.tsx
@@ -3,9 +3,25 @@ import { Button } from '@/components/ui/button';
 import { ThemeToggle } from '../theme-toggle';
 import { useTranslation } from 'react-i18next';
 import { LangToggle } from '../lang-toggle';
+import { useMediaQuery } from '@/utils/useMediaQuery';
+import { BREAKPOINTS } from '@/config';
 
 export const NoAuthNav = () => {
     const { t } = useTranslation();
+
+    const isSmall = useMediaQuery(BREAKPOINTS.sm);
+
+    if (isSmall) {
+        return (
+            <nav className="flex mr-2 items-center gap-1">
+                <LangToggle />
+                <ThemeToggle />
+                <Button asChild data-testid="sign-in-button">
+                    <Link to="/sign-in">{t('Sign in')}</Link>
+                </Button>
+            </nav>
+        );
+    }
 
     return (
         <nav className="flex mr-4 items-center gap-1">
